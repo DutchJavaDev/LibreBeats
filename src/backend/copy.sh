c04c7f1c2c912241024aa9515398a408ee6e883c
ABoredDeveloper <Borismulder@outlook.com>
2026-01-16 21:54:36 +0100

    Update copy.sh

--- src/backend/copy.sh
+++ src/backend/copy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# Build script for supabase backend
+# restarts the migratrions service to apply new changes
 source ./variables.sh
-
 _WORKING_DIR=$(pwd)
 
 cp -rf $_WORKING_DIR/supabase/* "$BUILD_DIRECTORY"
 
 # # # Switch to your project directory
-cd "$BUILD_DIRECTORY"
+cd "$BUILD_DIRECTORY/service/migration" || exit 1
 
 docker compose build migrations
-docker compose up migrations --force-recreate
+docker compose up migrations -d --force-recreate

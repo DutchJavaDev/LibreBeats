908c74f596ce9fd8e2a62b5954428d9b1efb1a05
ABoredDeveloper <Borismulder@outlook.com>
2026-03-09 23:18:05 +0100

    Create storage.go

---
+++ src/backend/supabase/service/audio/storage.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+
+	storage "github.com/supabase-community/storage-go"
+)
+
+type IStorageService interface {
+	UploadAudioFile(filePath string, fileName string) (storage.FileUploadResponse, error)
+	UploadImageFile(filePath string, fileName string) (storage.FileUploadResponse, error)
+}
+
+type StorageService struct {
+	IStorageService
+	client *storage.Client
+}
+
+func NewStorageService() *StorageService {
+	// Will throw an error if its missing a method implementation from interface
+	// will throw a compile time error
+	var _ IStorageService = (*StorageService)(nil)
+
+	storageUrl := os.Getenv("STORAGE_URL")
+
+	if storageUrl == "" {
+		panic("STORAGE_URL environment variable is not set")
+	}
+
+	storageKey := os.Getenv("STORAGE_KEY")
+
+	if storageKey == "" {
+		panic("STORAGE_KEY environment variable is not set")
+	}
+
+	storageClient := storage.NewClient(storageUrl, storageKey, nil)
+
+	return &StorageService{
+		client: storageClient,
+	}
+}
+
+func (s *StorageService) UploadAudioFile(filePath string, fileName string) (storage.FileUploadResponse, error) {
+	// Open the file you want to upload
+	file, err := os.Open(filePath)
+	if err != nil {
+		return storage.FileUploadResponse{}, err
+	}
+	defer file.Close()
+
+	// Upload the file to the specified bucket and path
+	response, err := s.client.UploadFile("audio-files", fileName, file)
+
+	if err != nil {
+		return storage.FileUploadResponse{}, err
+	}
+	return response, nil
+}
+
+func (s *StorageService) UploadImageFile(filePath string, fileName string) (storage.FileUploadResponse, error) {
+	// Open the file you want to upload
+	file, err := os.Open(filePath)
+	if err != nil {
+		return storage.FileUploadResponse{}, err
+	}
+	defer file.Close()
+
+	// Upload the file to the specified bucket and path
+	response, err := s.client.UploadFile("image-files", fileName, file)
+
+	if err != nil {
+		return storage.FileUploadResponse{}, err
+	}
+	return response, nil
+}

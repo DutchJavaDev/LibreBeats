91df9d6d279fbc7f413d8a52a27209f94820f0d8
ABoredDeveloper <Borismulder@outlook.com>
2026-03-11 20:53:43 +0100

    Update 0 initial.sql

--- src/backend/supabase/service/migration/scripts/0 initial.sql
+++ src/backend/supabase/service/migration/scripts/0 initial.sql
@@ -20,26 +20,35 @@ GRANT ALL ON ALL SEQUENCES IN SCHEMA Librebeats TO service_role;
 -- Create the audio processing queue
 SELECT * FROM pgmq.create('audiopipe-input');
 
--- Example insert into the queue
--- SELECT * FROM pgmq.send('audiopipe-input', '{"key": "path/to/audio/file.mp3", "metadata": {"artist": "Artist Name", "album": "Album Name"}}', 0);
-
 CREATE TABLE IF NOT EXISTS Librebeats.Audio (
     Id SERIAL PRIMARY KEY,
-    SourceId TEXT NOT NULL,
-    SourceName TEXT NOT NULL,
-    StorageLocation TEXT,
-    ThumbnailLocation TEXT,
+    AudioLocation TEXT NOT NULL,
+    ThumbnailLocation TEXT NOT NULL,
     DownloadCount INT NOT NULL DEFAULT 0,
-    CreatedAtUtc TIMESTAMP NOT NULL DEFAULT NOW()
+    CreatedAtUtc TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 ALTER TABLE Librebeats.Audio ENABLE ROW LEVEL SECURITY;
 
--- ONLY authenticated users can access all the Audio table
-CREATE POLICY "Authenticated users can access all audio" ON Librebeats.Audio
+CREATE TABLE IF NOT EXISTS Librebeats.Song (
+    Id SERIAL PRIMARY KEY,
+    AudioId SERIAL NOT NULL REFERENCES Librebeats.Audio(Id) ON DELETE CASCADE,
+    Title TEXT NOT NULL,
+    Artist TEXT NOT NULL,
+    Album TEXT NOT NULL,
+    Tags TEXT NOT NULL,
+    StreamingUrl TEXT NOT NULL,
+    ThumbnailUrl TEXT NOT NULL,
+);
+
+ALTER TABLE Librebeats.Song ENABLE ROW LEVEL SECURITY;
+
+-- ONLY authenticated users can access songs
+CREATE POLICY "Authenticated users can access all songs" ON Librebeats.Song
     FOR SELECT
     TO authenticated
     USING (true);
+
 
 CREATE TABLE IF NOT EXISTS Librebeats.YtdlpOutputLog (
     Id INT PRIMARY KEY,
@@ -47,14 +56,14 @@ CREATE TABLE IF NOT EXISTS Librebeats.YtdlpOutputLog (
     ProgressState INT NOT NULL,
     OutputBase64 TEXT NOT NULL,
     ErrorOutputBase64 TEXT,
-    StartedAtUtc TIMESTAMP NOT NULL DEFAULT NOW(),
-    FinishedAtUtc TIMESTAMP
+    StartedAtUtc TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    FinishedAtUtc TIMESTAMP WITH TIME ZONE
 );
 
 ALTER TABLE Librebeats.YtdlpOutputLog ENABLE ROW LEVEL SECURITY;
 
 -- Only service role can access the YtdlpOutputLog table
-CREATE POLICY "Service role can access YtdlpOutputLog" ON Librebeats.YtdlpOutputLog
+CREATE POLICY "Authenticated users can access YtdlpOutputLog" ON Librebeats.YtdlpOutputLog
     FOR SELECT
     TO authenticated
     USING (true);

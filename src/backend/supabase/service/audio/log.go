2b7b74b71f98c54016abc834b9e3238d0e48e726
ABoredDeveloper <Borismulder@outlook.com>
2026-03-15 17:31:07 +0100

    Update log.go

--- src/backend/supabase/service/audio/log.go
+++ src/backend/supabase/service/audio/log.go
@@ -1,38 +1,84 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
 
-type IYtdlpLogger interface {
-	CreateNewLog(title string) (*YtdlpOutputLog, error)
-	UpdateLog(log *YtdlpOutputLog) error
+	"github.com/jackc/pgx/v5"
+)
+
+type IAudioOutputLogger interface {
+	CreateNewLog(title string) (AudioOutput, error)
+	UpdateLog(log *AudioOutput) error
 }
 
-type YtdlpLogger struct {
-	IYtdlpLogger
-	database BaseTable
+type AudioOutputLogger struct {
+	IAudioOutputLogger
+	ConnectionString string
 }
 
-func NewYtdlpLogger() *YtdlpLogger {
+func NewYtdlpLogger() AudioOutputLogger {
 	// Will throw an error if its missing a method implementation from interface
 	// will throw a compile time error
-	var _ IYtdlpLogger = (*YtdlpLogger)(nil)
+	var _ IAudioOutputLogger = (*AudioOutputLogger)(nil)
 
-	return &YtdlpLogger{}
+	connectionString := os.Getenv("POSTGRES_BACKEND_URL")
+
+	if connectionString == "" {
+		panic("POSTGRES_BACKEND_URL environment variable is not set")
+	}
+
+	return AudioOutputLogger{
+		ConnectionString: connectionString,
+	}
 }
 
-func (l *YtdlpLogger) CreateNewLog(title string) (*YtdlpOutputLog, error) {
-	lastinsertedId, err := l.database.InsertWithReturningIdUUID("INSERT INTO YtdlpOutputLog (Title, ProgressState) VALUES ($1, $2) RETURNING id", title, Created)
+func (l *AudioOutputLogger) CreateNewLog(title string) (AudioOutput, error) {
+	connection, err := pgx.Connect(context.Background(), l.ConnectionString)
+
 	if err != nil {
-		return nil, err
+		return AudioOutput{}, err
 	}
-	return &YtdlpOutputLog{
+
+	defer connection.Close(context.Background())
+
+	var lastinsertedId = 1
+
+	trans, err := connection.Begin(context.Background())
+
+	scanError := trans.QueryRow(context.Background(), "INSERT INTO Librebeats.AudioOutputLog (Title, ProgressState) VALUES ($1, $2) RETURNING id", title, Created).Scan(&lastinsertedId)
+
+	if scanError != nil {
+		return AudioOutput{}, err
+	}
+
+	trans.Commit(context.Background())
+
+	return AudioOutput{
 		Id:            lastinsertedId,
 		ProgressState: int(Created),
-		Title:         title,
+		Title:         &title,
 	}, nil
 }
 
-func (l *YtdlpLogger) UpdateLog(log *YtdlpOutputLog) error {
-	_, err := l.database.Pool.Exec(context.Background(), "UPDATE YtdlpOutputLog SET Title = $1, Output = $2, ErrorOutput = $3, ProgressState = $4, FinishedAtUtc = $5 WHERE id = $6", log.Title, log.OutputLog, log.ErrorOutputLog, log.ProgressState, log.FinishedAtUtc, log.Id)
-	return err
+func (l *AudioOutputLogger) UpdateLog(log *AudioOutput) error {
+	connection, err := pgx.Connect(context.Background(), l.ConnectionString)
+
+	if err != nil {
+		return err
+	}
+	defer connection.Close(context.Background())
+
+	trans, err := connection.Begin(context.Background())
+
+	_, transError := trans.Exec(context.Background(), "UPDATE Librebeats.AudioOutputLog SET Title = $1, Output = $2, ErrorOutput = $3, ProgressState = $4, FinishedAtUtc = $5 WHERE id = $6", log.Title, log.Output, log.ErrorOutput, log.ProgressState, log.FinishedAtUtc, log.Id)
+
+	if transError != nil {
+		fmt.Println(transError.Error())
+	}
+
+	trans.Commit(context.Background())
+
+	return transError
 }

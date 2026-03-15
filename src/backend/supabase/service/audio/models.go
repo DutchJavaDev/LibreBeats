ac681bad6665169ef20e4106eab291c8b7b09ba1
ABoredDeveloper <Borismulder@outlook.com>
2026-03-15 17:31:22 +0100

    Update models.go

--- src/backend/supabase/service/audio/models.go
+++ src/backend/supabase/service/audio/models.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/json"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type ProgressState int
@@ -26,26 +24,32 @@ type AudioProcessingMessage struct {
 	Url string `json:"url"`
 }
 
-type Audio struct {
-	Id                int       `json:"id" db:"id"`
-	Title             string    `json:"title" db:"title"`
-	Artist            string    `json:"artist" db:"artist"`
-	Album             string    `json:"album" db:"album"`
-	AudioLocation     string    `json:"audioLocation" db:"audio_location"`
-	ThumbnailLocation string    `json:"thumbnailLocation" db:"thumbnail_location"`
-	StreamingURL      string    `json:"streamingUrl" db:"streaming_url"`
-	ThumbnailURL      string    `json:"thumbnailUrl" db:"thumbnail_url"`
-	DownloadCount     int       `json:"downloadCount" db:"download_count"`
-	CreatedAtUTC      time.Time `json:"createdAtUtc" db:"created_at_utc"`
+type Beat struct {
+	Id           int     `json:"id" db:"id" gorm:"primaryKey;autoIncrement"`
+	RawBeatId    int     `json:"raw_beat_id" db:"raw_beat_id" gorm:"not null;index"` // foreign key to Audio/RawBeat
+	Title        *string `json:"title" db:"title" gorm:"not null"`
+	Artist       *string `json:"artist" db:"artist" gorm:"not null"`
+	Tags         *string `json:"tags" db:"tags" gorm:"not null"`
+	StreamingURL *string `json:"streaming_url" db:"streaming_url" gorm:"not null"`
+	ThumbnailURL *string `json:"thumbnail_url" db:"thumbnail_url" gorm:"not null"`
 }
 
-type YtdlpOutputLog struct {
-	Id             uuid.UUID `db:"id"`
-	AudioId        uuid.UUID `db:"audio_id"`
-	ProgressState  int       `db:"progress_state"`
-	Title          string    `db:"title"`            // nullable
-	OutputLog      string    `db:"output_log"`       // nullable
-	ErrorOutputLog string    `db:"error_output_log"` // nullable
-	CreatedAtUtc   time.Time `db:"created_at_utc"`
-	FinishedAtUtc  time.Time `db:"finished_at_utc"` // nullable
+type RawBeat struct {
+	Id                int       `json:"id" db:"id" gorm:"primaryKey;autoIncrement"`
+	Source            *string   `json:"source" db:"source" gorm:"not null"`
+	AudioLocation     *string   `json:"audio_location" db:"audio_location" gorm:"not null"`
+	ThumbnailLocation *string   `json:"thumbnail_location" db:"thumbnail_location" gorm:"not null"`
+	DownloadCount     int       `json:"download_count" db:"download_count" gorm:"not null;default:0"`
+	CreatedAtUtc      time.Time `json:"created_at_utc" db:"created_at_utc" gorm:"not null;default:now()"`
+	Duration          int       `json:"duration" db:"duration" gorm:"not null;default 0"`
+}
+
+type AudioOutput struct {
+	Id            int        `json:"id" db:"id" gorm:"primaryKey;autoIncrement"`
+	Title         *string    `json:"title" db:"title" gorm:"not null"`
+	ProgressState int        `json:"progress_state" db:"progress_state" gorm:"not null"`
+	Output        *string    `json:"output,omitempty" db:"output" gorm:"default:null"`             // nullable
+	ErrorOutput   *string    `json:"error_output,omitempty" db:"error_output" gorm:"default:null"` // nullable
+	StartedAtUtc  *time.Time `json:"started_at_utc" db:"started_at_utc" gorm:"not null;default:now()"`
+	FinishedAtUtc time.Time  `json:"finished_at_utc,omitempty" db:"finished_at_utc" gorm:"default:null"` // nullable
 }

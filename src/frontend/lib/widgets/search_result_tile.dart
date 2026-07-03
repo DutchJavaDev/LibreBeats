828704353c4c422554871b2f7a7b635934dd23e9
ABoredDeveloper <Borismulder@outlook.com>
2026-07-03 19:03:02 +0200

    Update search_result_tile.dart

--- src/frontend/lib/widgets/search_result_tile.dart
+++ src/frontend/lib/widgets/search_result_tile.dart
@@ -1,18 +1,17 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:liberated_beats/main.dart';
 import 'package:liberated_beats/models/beat_models.dart';
+import 'package:liberated_beats/providers/player_provider.dart';
+import 'package:liberated_beats/widgets/track_tile.dart';
+import 'package:liberated_beats/widgets/widget_builder.dart';
+import 'package:provider/provider.dart';
 
 class SearchTile extends StatelessWidget {
   final SearchResult search;
-  final bool isActive;
-  final bool isPlaying;
-  final VoidCallback onTap;
 
-  const SearchTile(
-      {super.key,
-      required this.onTap,
-      required this.search,
-      required this.isActive,
-      required this.isPlaying});
+  const SearchTile({super.key, required this.search});
 
   String _format(Duration d) {
     final m = d.inMinutes;
@@ -22,44 +21,79 @@ class SearchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final player = context.read<PlayerProvider>();
+    final isActive = player.currentTrack?.id == search.beat?.id;
 
-    if(search.beatMix != null){
+    if (search.beatMix != null) {
       final beatMix = search.beatMix!;
-      return ListTile(
-        onTap: onTap,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-        leading: Image.network(
-          beatMix.thumbnailUrl,
-          width: 52,
-          height: 52,
-          fit: BoxFit.cover,
-        ),
-        title: Text(
-          beatMix.title,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w600,
-            color: isActive ? const Color(0xFF1ED760) : Colors.white,
+      PrintLog('beatMix: ${beatMix.title}, tracks: ${beatMix.beats?.length}');
+      return Stack(
+        children: [
+          Positioned.fill(
+            child: ClipRect(
+              child: ImageFiltered(
+                imageFilter: ui.ImageFilter.blur(
+                    sigmaX: 2.5, // Adjust horizontal blur strength
+                    sigmaY: 2.5, // Adjust vertical blur strength
+                    tileMode: TileMode.clamp),
+                child: createCachedNetworkImage(
+                  imageUrl: beatMix.thumbnailUrl,
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
-        ),
-        subtitle: Text(
-          '${beatMix.trackCount} tracks',
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(fontSize: 12, color: Color(0xFFA7A7A7)),
-        ),
-        trailing: const Icon(Icons.more_vert, color: Color(0xFFA7A7A7), size: 18),
+
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withValues(alpha: 0.4),
+            ),
+          ),
+
+          // 3. Your original ListTile (transparent by default)
+          ListTile(
+            onTap: () => showBeatMixDialog(context, beatMix),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            title: Text(
+              beatMix.title,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: isActive
+                    ? const Color(0xFF1ED760)
+                    : Colors
+                        .white, // TODO need to check if the beat is part of the beatmix instead
+              ),
+            ),
+            subtitle: Text(
+              '${beatMix.trackCount} songs',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  fontSize: 14,
+                  color: ui.Color.fromARGB(255, 255, 255, 255),
+                  fontWeight: FontWeight.bold),
+            ),
+            // trailing: IconButton(onPressed: (){
+            //       // TODO Play 
+            //     }, icon: const Icon(Icons.play_arrow, color: ui.Color.fromARGB(255, 29, 185, 84), size: 32)),
+          ),
+        ],
       );
-    }
-    else if (search.beat != null) {
+    } else if (search.beat != null) {
       final beat = search.beat!;
       return ListTile(
-        onTap: onTap,
+        onTap: () {
+          player.playTrack(beat);
+        },
         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-        leading: Image.network(
-          beat.album,
+        leading: createCachedNetworkImage(
+          imageUrl: beat.album,
           width: 52,
           height: 52,
           fit: BoxFit.cover,
@@ -85,84 +119,19 @@ class SearchTile extends StatelessWidget {
           children: [
             Text(
               _format(beat.duration),
-              style:
-                  const TextStyle(fontSize: 13, color: Color(0xFFA7A7A7)),
+              style: const TextStyle(fontSize: 13, color: Color(0xFFA7A7A7)),
             ),
             const SizedBox(width: 4),
             const Icon(Icons.more_vert, color: Color(0xFFA7A7A7), size: 18),
           ],
         ),
       );
+    } else {
+      return const Center(
+          child: Text(
+        "No results found",
+        style: TextStyle(color: Colors.white),
+      ));
     }
-    else
-    {
-      return Center(child: Text("No results found", style: TextStyle(color: Colors.white),));
-    }
-
-
-    // return ListTile(
-    //   onTap: onTap,
-    //   contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-    //   leading: Stack(
-    //     children: [
-    //       Container(
-    //         width: 48,
-    //         height: 48,
-    //         alignment: Alignment.center,
-    //         decoration: BoxDecoration(
-    //           gradient: beat.color,
-    //           borderRadius: BorderRadius.circular(8),
-    //         ),
-    //         child: Text(
-    //           beat.title.isNotEmpty ? beat.title[0] : '?',
-    //           style: const TextStyle(
-    //               fontWeight: FontWeight.bold, color: Colors.white),
-    //         ),
-    //       ),
-    //       if (isActive && !isBeatMix)
-    //         Positioned.fill(
-    //           child: Container(
-    //             alignment: Alignment.center,
-    //             decoration: BoxDecoration(
-    //               color: Colors.black.withValues(alpha: 0.45),
-    //               borderRadius: BorderRadius.circular(8),
-    //             ),
-    //             child: Icon(
-    //               isPlaying ? Icons.pause : Icons.play_arrow,
-    //               color: Colors.white,
-    //               size: 22,
-    //             ),
-    //           ),
-    //         ),
-    //     ],
-    //   ),
-    //   title: Text(
-    //     beat.title,
-    //     maxLines: 1,
-    //     overflow: TextOverflow.ellipsis,
-    //     style: TextStyle(
-    //       fontSize: 14,
-    //       fontWeight: FontWeight.w600,
-    //       color: isActive ? const Color(0xFF1ED760) : Colors.white,
-    //     ),
-    //   ),
-    //   subtitle: Text(
-    //     beat.artist,
-    //     maxLines: 1,
-    //     overflow: TextOverflow.ellipsis,
-    //     style: const TextStyle(fontSize: 12, color: Color(0xFFA7A7A7)),
-    //   ),
-    //   trailing: Row(
-    //     mainAxisSize: MainAxisSize.min,
-    //     children: [
-    //       Text(
-    //         _format(beat.duration),
-    //         style: const TextStyle(fontSize: 13, color: Color(0xFFA7A7A7)),
-    //       ),
-    //       const SizedBox(width: 4),
-    //       const Icon(Icons.more_vert, color: Color(0xFFA7A7A7), size: 18),
-    //     ],
-    //   ),
-    // );
   }
 }

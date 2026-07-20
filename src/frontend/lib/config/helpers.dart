5d7eca1882bdd7b0b3b7cb58253ca1afb1312d9d
ABoredDeveloper <Borismulder@outlook.com>
2026-07-20 23:41:45 +0200

    Update helpers.dart

--- src/frontend/lib/config/helpers.dart
+++ src/frontend/lib/config/helpers.dart
@@ -1,3 +1,3 @@
 import 'package:liberated_beats/models/beat_models.dart';
 
-typedef VoidCallbackDoulbe = void Function(double,Beat);
+typedef VoidCallbackUpdateProgress = void Function(double,Beat);

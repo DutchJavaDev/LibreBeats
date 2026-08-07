086b1839e500041d3ec15cb9577044134299e965
ABoredDeveloper <Borismulder@outlook.com>
2026-08-07 11:02:34 +0200

    Update helpers.dart

--- src/frontend/lib/config/helpers.dart
+++ src/frontend/lib/config/helpers.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:liberated_beats/models/beat_models.dart';
 
-typedef VoidCallbackUpdateProgress = void Function(double,Beat);
+typedef VoidCallbackUpdateProgress = void Function(double, Beat);
+
+// ignore: non_constant_identifier_names
+void PrintLog(Object? object) {
+  if (kDebugMode) {
+    print("[LIBRE-BEATS]: $object");
+  } else {
+    // Write to a log file or send to a logging service in production
+  }
+}

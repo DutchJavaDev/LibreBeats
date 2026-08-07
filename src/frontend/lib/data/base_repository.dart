9cf4faec954f01d3bd6d8dba21ce290e65f03b38
ABoredDeveloper <Borismulder@outlook.com>
2026-08-07 11:02:46 +0200

    Update base_repository.dart

--- src/frontend/lib/data/base_repository.dart
+++ src/frontend/lib/data/base_repository.dart
@@ -1,11 +1,9 @@
 import 'package:liberated_beats/data/server_registry.dart';
 
-/// Shared base for repositories: access to the multi-server [ServerRegistry].
 class BaseRepository {
   BaseRepository(this.registry);
 
   final ServerRegistry registry;
 
-  /// Whether at least one server is signed in and reachable.
   bool get isConnected => registry.healthy.isNotEmpty;
 }
